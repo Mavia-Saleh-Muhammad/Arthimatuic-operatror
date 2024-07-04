18434e5fc61ea9ddf93bb9e7b98df3ba7332f307
Mavi <160594705+maviasalemuhmmad@users.noreply.github.com>
2024-07-04 08:48:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+"use strict";
+// arithmetic opretars
+// let numberofFaculty:number= 4;
+// let numberofManagemint: number =3;
+// let numberofAbsentFaculty =2;
+//let totalWeightofBaryani = 20
+//let totalNumberofFaculty =84
+// template litral
+// `Backtics`
+console.log(`how are you ${}`);
+//console.log ( totalWeightofBaryani /  totalNumberofFaculty);
+//let totalWeightofBaryani =70;//70 kg
+//let heightInMeters = 1.75; //1.75m
+//let bmi = weightInkg /(heightInMeters)
+//let firsttName = "mavia";
+//let age =22;
+// `Backtics`
+// let message =` hello my name is: ${firsttName} and my age is : ${age}`
+// console.log(message)
+// unary opretors
+//let index = 4;
+//++index 
+//++index;//5 
+//--index;//6
+//++index;//7
+//--index;//8
+//++index;//9
+//console.log (index)
+//what is this diffec
+// 11 + 12 + 8 =40 
+// let index =10;
+// let result = ++index + ++index + 8 + --index -2 
+// console.log(result);
+// let num1 = 20 ;
+// num1 -= 22
+// console.log(num1)
+// num1 = num1 - 22
+// comparison operators
+// let num1 = 20; 
+// let num2 = 50 ;
+// console.log (num1 == num2 )
+// let num1 = 20;
+// let num2 =40;
+// console.log(num1 != num2 )
+// let num1 = 20;
+// let num2 =20
+// console.log (num1 > num2)
+// let age = 18 ;
+// console.log (age >= 18 )
+// let age =18 ;
+// console .log (age <= 18)
